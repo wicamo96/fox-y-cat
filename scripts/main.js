015ddd3955c5d9b76504327a4d93b3e3572a8838
Will Moran <will.c.moran@gmail.com>
2024-05-06 15:37:31 -0400

    Added an async/await function that gets a random cat photo

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,14 @@ fetch('https://randomfox.ca/floof')
             foxImgElement.src = convertedData.image
         }
     )
+
+// Can be done without defining a function. Same functionality if you remove lines 11 and 17-19
+const getTheCat = async () => {
+    const response = await fetch('http://shibe.online/api/cats?count=1&urls=true&httpsUrls=false')
+    const convertedData = await response.json()
+
+    const catImgElement = document.querySelector("#cat")
+    catImgElement.src = convertedData[0]
+}
+
+getTheCat()
